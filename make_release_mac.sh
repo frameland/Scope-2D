7c28d945a0d14b68784b529de5d249972d145bfc
markus <frameland@users.noreply.github.com>
2018-03-15 18:25:43 +0100

    Update make_release_mac.sh

--- make_release_mac.sh
+++ make_release_mac.sh
@@ -8,7 +8,7 @@ mkdir Scope2DMac/source
 mkdir Scope2DMac/source/ressource
 
 #copy stuff (change )
-DEV_DIR="/Users/markus/Developer/Projects/Scope2D"
+DEV_DIR="/Users/yourname/Scope2D"
 cp -r $DEV_DIR/main.app/ Scope2DMac/Scope2D.app
 cp -r $DEV_DIR/source/ressource/ Scope2DMac/source/ressource/
 cp -r $DEV_DIR/data Scope2DMac/data
